c84886f45351f2ed5ff6cd8f3fa51c5600d0d098
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-03-20 19:28:00 -0700

    Testing Complete\

--- client/src/pages/TopicNews.js
+++ client/src/pages/TopicNews.js
@@ -1,27 +1,13 @@
-import axios from 'axios'
-import React, { useState } from 'react'
+import React from 'react'
 import CustomTitle from '../components/CustomTitle'
 
 function TopicNews() {
-    const [news, setNews] = useState([]);  
-    const makeReq = async () => {
-    const res = await axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=69B37vF2XwUQnMPhUn9NB8GOJEe0ptNg")
-    setNews(res.data.response.docs);
-    }
-
-    React.useEffect(() => {
-        makeReq()
-    }, [])
+    
 
     return (
         <div>
             <CustomTitle page="News"/>
-            {
-                news.map(item => {
-                    return <p>{item.abstract}</p>
-
-                })
-            }
+            
         </div>
     )
 }
