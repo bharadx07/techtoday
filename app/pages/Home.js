785dc7bf0027caea0a9b115cfa71c5f536e595f0
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-04-15 22:55:11 -0700

    app home worked on

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -1,54 +1,71 @@
 import React from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
+import {
+  FontAwesome,
+  Entypo,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
-
-const Home = () => {
+const Home = ({ navigation }) => {
   return (
     <ScrollView style={styles.main}>
       <Text style={styles.name}>TechToday</Text>
-      <Text>
+      <Text style={styles.tagLine}>
         TechToday is the Source for Technology Related News and Jobs in Many
         Industries
       </Text>
-      <Text>Your Personal Hub</Text>
-      <Text>All the Current Tech Information You Want</Text>
-      <View> 
-        <Text>News</Text>
-        <Text>
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <Text
+          style={styles.tryNow}
+          onPress={() => {
+            navigation.navigate("Register");
+          }}
+        >
+          Try TechToday Now
+        </Text>
+      </View>
+      <Text style={styles.hub}>Your Personal Hub</Text>
+      <Text style={styles.hubTagLine}>
+        All the Current Tech Information You Want
+      </Text>
+      <View>
+        <Text style={styles.productTitle}>News</Text>
+        <Text style={styles.productTypeDescription}>
           Catch up with all the current events taking place in many industries.
           Learn about the news by reading quick news snippets, or if you are
           instrested, view the full article with one click.
         </Text>
-        <Text>Icon</Text>
+        <FontAwesome name="newspaper-o" size={84} style={styles.icon} />
       </View>
       <View>
-        <Text>Jobs</Text>
-        <Text>
+        <Text style={styles.productTitle}>Jobs</Text>
+        <Text style={styles.productTypeDescription}>
           Wanting to find a job in a certain topic? Check the job section of
           certain field and view the availible jobs near you in that industry
         </Text>
-        <Text>Icon</Text>
+        <Entypo name="briefcase" size={84} style={styles.icon} />
       </View>
-      <View>
-        <Text>Topics</Text>
-        <Text>
+      <View style={styles.topics}>
+        <Text style={styles.topicsTitle}>Topics</Text>
+        <Text style={styles.topicsTagLine}>
           Explore information in Tech, Buisness and Other Related Fields
         </Text>
-        <View>
-          <Text>Icon</Text>
-          <Text>Software</Text>
+        <View style={styles.difTopics}>
+          <Entypo name="code" size={64} style={styles.difTopicIcon} />
+          <Text style={styles.difTopicame}>Software</Text>
         </View>
         <View>
-          <Text>Icon</Text>
-          <Text>Hardware</Text>
+          <Ionicons name="hardware-chip-sharp" size={64} style={styles.difTopicIcon} />
+          <Text style={styles.difTopicName}>Hardware</Text>
         </View>
         <View>
-          <Text>Icon</Text>
-          <Text>Finance</Text>
+          <MaterialCommunityIcons name="finance" size={64} style={styles.difTopicIcon} />
+          <Text style={styles.difTopicName}>Finance</Text>
         </View>
         <View>
-          <Text>Icon</Text>
-          <Text>Buisness</Text>
+          <Ionicons name="business" size={64} style={styles.difTopicIcon} />
+          <Text style={styles.difTopicName}>Buisness</Text>
         </View>
       </View>
       <View>
@@ -63,34 +80,119 @@ const Home = () => {
         </View>
         <View>
           <Text>Clean User Interface</Text>
-          <Text>TechToday has a Clean User Interface that allows you to find the information you need without trouble or confusion</Text>
+          <Text>
+            TechToday has a Clean User Interface that allows you to find the
+            information you need without trouble or confusion
+          </Text>
         </View>
       </View>
       <View>
-        <Text>Ready to Try <Text>TechToday</Text></Text>
+        <Text>
+          Ready to Try <Text>TechToday</Text>
+        </Text>
         <Text>Create An Account</Text>
       </View>
       <View>
-        <Text>&copy; TechToday 2021 | All Rights Reserved</Text> 
+        <Text>&copy; TechToday 2021 | All Rights Reserved</Text>
       </View>
     </ScrollView>
   );
-}; 
+};
+
+const PRIMARY_COLOR = "#cb4745";
 
 const styles = StyleSheet.create({
   main: {
     textAlign: "center",
-
   },
 
   name: {
-    color: "red",
-    margin: "1rem auto"
-  }
-  
-})
+    color: PRIMARY_COLOR,
+    marginTop: "1rem",
+    fontSize: "40px",
+  },
 
+  tagLine: {
+    fontSize: "1rem",
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
 
+  tryNow: {
+    fontWeight: "bold",
+    fontSize: "17px",
+    cursor: "pointer",
+    border: "none",
+    outline: "none",
+    color: "#fff",
+    backgroundColor: PRIMARY_COLOR,
+    padding: 10,
+    borderRadius: "4px",
+    textAlign: "center",
+    marginTop: "1rem",
+  },
+
+  hub: {
+    fontSize: "1rem",
+    color: PRIMARY_COLOR,
+    fontWeight: "bold",
+    marginTop: 20,
+  },
+
+  hubTagLine: { fontWeight: "normal", marginTop: 5, fontSize: 16 },
+
+  productTitle: { fontWeight: "bold", fontSize: 25, marginTop: 10 },
+
+  productTypeDescription: { paddingLeft: 20, paddingRight: 20 },
+
+  icon: { color: PRIMARY_COLOR, marginTop: 15 },
+
+  topics: {
+    backgroundColor: "lightgray",
+    marginTop: 15,
+  },
+
+  topicsTitle: {
+    marginTop: 10,
+    fontWeight: "bold",
+    fontSize: 25,
+  },
+
+  topicsTagLine: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginTop: 10,
+  },
+
+  difTopics: {
+    marginTop: 5,
+  },
+
+  difTopicIcon: {
+    color: PRIMARY_COLOR,
+  },
+
+  difTopicName: {},
+
+  vallTopics: {},
+
+  featuresTitle: {},
+
+  featureType: {},
+
+  featureParagraph: {},
+
+  ctaWrap: {},
+
+  ctaTitle: {},
+
+  ctaSpecial: {},
+
+  ctaBTN: {},
+
+  footer: {},
+
+  footerText: {},
+});
 
 export default Home;
- 
